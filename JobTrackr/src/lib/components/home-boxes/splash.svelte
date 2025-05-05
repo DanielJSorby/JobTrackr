471fb5e160729f020d3da16a30816cc3c0c21e3c
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2025-05-05 08:54:44 +0200

    Lagde splash side

---
+++ JobTrackr/src/lib/components/home-boxes/splash.svelte
@@ -0,0 +1,67 @@
+<div class="splash">
+    <div class="splash-content">
+        <div class="text-content">
+            <h1 class="header">
+                Organize Your<br>Job Search
+            </h1>
+        <p class="subheader">
+            Keep track of all your job applications in one place. Never miss a follow-up or lose track of your progress.
+        </p>
+        </div>
+        <div class="button-container">
+            <a href="/signup"><button class="button filled">Get Started <img src="/icons/arrow-right.svg" alt="->"></button></a>
+            <a href="/login"><button class="button transparent">Log In</button></a>
+        </div>
+    </div>
+    <div class="image-container">
+        <img id="splash-image" src="https://www.betterteam.com/images/betterteam-best-job-boards-6416x4277-2022095.jpeg?crop=1:1,smart&width=1200&dpr=2&format=pjpg&auto=webp&quality=85" alt="JobTrackr">
+    </div>
+</div>
+
+<style>
+    .splash {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        height: 100vh;
+        width: 100vw;
+    }
+
+    .splash-content {
+        width: 40vw;
+    }
+
+    .button-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding-top: 32px;
+        gap: 16px;
+    }
+    
+    .header {
+        font-size: 92px;
+        font-weight: 600;
+    }
+
+    .subheader {
+        font-size: 24px;
+        font-weight: 400;
+        color: #6B7280;
+    }
+
+    .image-container {
+        width: 40vw;
+        border-radius: 16px;
+        overflow: hidden;
+    }
+
+    #splash-image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 16px;
+    }
+    
+</style>
--- JobTrackr/src/routes/+page.svelte
+++ JobTrackr/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Splash from '$lib/components/home-boxes/splash.svelte';
+</script>
+
+<Splash />
