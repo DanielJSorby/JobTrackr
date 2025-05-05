e74bf1855aae13814616ad7913f0feebcf2d11f4
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2025-05-05 21:58:57 +0200

    oppdaterte navlinks på ulike sider

--- JobTrackr/src/routes/+layout.svelte
+++ JobTrackr/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import type { Snippet } from 'svelte';
+    import { page } from '$app/stores';
     import type { LayoutData } from './$types';
     import '$lib/styles/global.css';
     import Navbar from '$lib/components/navbar.svelte';
@@ -7,6 +8,10 @@
     let { data, children }: { data: LayoutData, children: Snippet } = $props();
 </script>
 
-<Navbar />
+{#if $page.route.id == '/login' || $page.route.id == '/signup' || $page.route.id == '/'}
+    <Navbar />
+    {:else}
+    <Navbar links={[{ label: 'Dashboard', href: '/dashboard' }, { label: 'Logout', href: '/logout' }]}/>
+{/if}
 
 {@render children()}
