63bb5cf0e71f3f99fb1d131ff294ff943e84ea6f
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2025-05-06 22:00:57 +0200

    la til google ads

--- JobTrackr/src/routes/+layout.svelte
+++ JobTrackr/src/routes/+layout.svelte
@@ -15,6 +15,13 @@
     let { data, children }: { data: LayoutData, children: Snippet } = $props();
 </script>
 
+<svelte:head>
+    <title>JobTrackr</title>
+    <meta name="description" content="JobTrackr is a tool for tracking your job applications.">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1582322793713299"
+     crossorigin="anonymous"></script>
+</svelte:head>
+
 {#if $page.route.id == '/login' || $page.route.id == '/signup' || $page.route.id == '/'}
     <Navbar />
     {:else}
