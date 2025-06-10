303982694b057e63f6ac487da8c8ea9e683e977d
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2025-06-10 17:37:05 +0200

    la til error side

---
+++ JobTrackr/src/routes/+error.svelte
@@ -0,0 +1,52 @@
+<script>
+    import { page } from '$app/stores';
+
+    let errors = {
+        404: {
+            message: 'You\'re lost, go back to the homepage'
+        },
+        500: {
+            message: 'Something went wrong, It\'s not you, it\'s us'
+        }
+    }
+</script>
+
+<svelte:head>
+    <title>{$page.status} error</title>
+</svelte:head>
+
+<div class="bg">
+    <h1 class="accent-blue xbold">{$page.status}</h1>
+    {#if errors[$page.status]}
+        <p>{errors[$page.status].message}</p>
+    {:else}
+        <p>{$page.error.message}</p>
+    {/if}
+    <a href="/"><button class="filled button">Go back to Home</button></a>
+</div>
+
+<style>
+    .bg {
+        background-color: var(--background);
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 32px;
+    }
+
+    .bg h1 {
+        font-size: clamp(2rem, 20vw, 25rem);
+        color: #1D40B0;
+    }
+
+    .bg p {
+        color: #000;
+        font-size: clamp(1rem, 5vw, 20rem);
+    }
+
+    .bg button {
+        margin-top: 32px;
+    }
+</style>
